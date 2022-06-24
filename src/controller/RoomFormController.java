77f11d61993d1ce48bed4e5883fb5132fbf22570
kavindu1thiwanka <kavidu12thiwanka@gmail.com>
2022-06-24 14:55:28 +0530

    RoomForm.fxml

--- src/controller/RoomFormController.java
+++ src/controller/RoomFormController.java
@@ -1,4 +1,71 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+
 public class RoomFormController {
+    public AnchorPane root;
+    public JFXTextField txtRoomId;
+    public TableView tblStudent;
+    public TableColumn colRoomId;
+    public TableColumn colRoomType;
+    public TableColumn colMonthlyRent;
+    public TableColumn colQty;
+    public JFXButton btnSave;
+    public JFXButton btnUpdate;
+    public JFXButton btnDelete;
+    public JFXComboBox cmbRoomType;
+    public JFXTextField txtQty;
+    public JFXTextField txtMonthlyRent;
+
+    public void navigateToHome(MouseEvent mouseEvent) throws IOException {
+        URL resource = this.getClass().getResource("/view/DashboardForm.fxml");
+        Parent root = FXMLLoader.load(resource);
+        Scene scene = new Scene(root);
+        Stage primaryStage = (Stage) (this.root.getScene().getWindow());
+        primaryStage.setScene(scene);
+        primaryStage.centerOnScreen();
+        Platform.runLater(() -> primaryStage.sizeToScene());
+    }
+
+    public void navigateToLogin(MouseEvent mouseEvent) throws IOException {
+        URL resource = this.getClass().getResource("/view/LoggingForm.fxml");
+        Parent root = FXMLLoader.load(resource);
+        Scene scene = new Scene(root);
+        Stage primaryStage = (Stage) (this.root.getScene().getWindow());
+        primaryStage.setScene(scene);
+        primaryStage.centerOnScreen();
+        Platform.runLater(() -> primaryStage.sizeToScene());
+    }
+
+    public void closeWindowOnAction(ActionEvent actionEvent) {
+        javafx.application.Platform.exit();
+    }
+
+    public void textFields_Key_Released(KeyEvent keyEvent) {
+    }
+
+    public void btnDelete_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnUpdate_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSave_OnAction(ActionEvent actionEvent) {
+    }
 }
